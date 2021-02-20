5432d36aa5c279ed2527c1aaf36d9d7105027911
Andrei Pauliukovich <apavlukovich@gmail.com>
2021-02-20 22:57:07 +0300

    init:start_task_keybord

---
+++ 13_Virtual-Keyboard/script.js
@@ -0,0 +1,25 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+
+    },
+
+    _createKeys() {
+
+    },
+}
